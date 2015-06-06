35de4264c23d524aee320eec604bf4ae4392458d
luisoscategui <luis.oscategui@xurface.com>
2015-06-05 22:20:44 -0500

    files

--- helloworld.js
+++ helloworld.js
@@ -1,8 +1,38 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
+
+function serverStaticFile(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function(err, data){
+    if(err){
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Error');
+    }else{
+      res.writeHead(responseCode, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+
+}
 
 http.createServer(function(req, res){
-  res.writeHead(200, { 'Content-type': 'text/plain' });
-  res.end('Hello Word');
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch (path) {
+    case '':
+      serverStaticFile(res, '/public/home.html', 'text/plain');
+      break;
+    case '/about':
+      serverStaticFile(res, '/public/about.html', 'text/plain');
+      break;
+    case '/images/logorci.svg':
+      serverStaticFile(res, '/public/images/logorci.svg', 'image/svg+xml');
+      break;
+    default:
+      serverStaticFile(res, '/public/notfound.html', 'text/html', 404);
+      break;
+
+  }
 }).listen(3000);
 
-console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
+
+console.log('Server started on localhost:3000');
